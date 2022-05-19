e32108f5b404a1fbe723aaec70cd79c76e32c031
masa555555 <matsuda763@gmail.com>
2022-05-18 20:09:38 -0700

    first commit

---
+++ frontend/static/js/views/CountryView.js
@@ -0,0 +1,39 @@
+import AbstractView from "./AbstractView.js";
+import { getCountries } from "../countries.js";
+
+export default class extends AbstractView{
+    constructor(params) {
+        super(params);
+        this.setTitle("Country");
+    }
+
+    async getHTML() {
+        let countries = getCountries()
+        // let country = countries.reduce((prev, curr)=> {
+        //     if(curr.cca3 == this.params.id){
+        //         return prev.concat(curr);
+        //     } else {
+        //         return prev;
+        //     }
+        // },[])[0];
+
+        let country;
+        for(let i=0; i<countries.length; i++) {
+            if(countries[i].cca3==this.params.id){
+                country = countries[i];
+            }
+        }
+        return `
+        <div class="countryV">
+        <h1>${country}'s flag is ${country.flag}</h1>
+        <p>Capital: ${country.capital}</p>
+        <p>Languages: ${country.languages}</p>
+        <p>Population: ${country.population}</p>
+        <p>Currencies: ${country.currencies}</p>
+        <p>Region: ${country.region}</p>
+        <p>Subregion: ${country.subregion}</p>
+        </div>
+        `;
+    }
+    
+}
---
+++ frontend/static/js/views/Homepage.js
@@ -0,0 +1,19 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView{
+    constructor(params) {
+        super(params);
+        this.setTitle("Homepage");
+    }
+
+    async getHTML() {
+        return `
+        <div style="background-color: red">
+            <button id="test">description</button>
+        </div>
+        
+<button onclick="myFunction()">Toggle dark mode</button>
+        `;
+    }
+}
+
